d3a25e591cea3bcc7a25e7c7f097dbe440416e17
TylerNguyen21 <tydnguyen@ucdavis.edu>
2018-11-26 20:53:49 -0800

    finished two passing tests

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,14 +1,16 @@
-import exampleVideoData from './src/data/exampleVideoData.js';
+import VideoListEntry from '/compiled/src/components/VideoListEntry.js';
 
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+var VideoList = (props) => {
+  console.log('props', props);
+  return (
+    <div className="video-list">
+      {props.videos.map(video => 
+        <VideoListEntry video={video}/>
+      )}
+    </div>
+    
+  );
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -19,3 +21,5 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
+// <div><h5><em>{props.snippet}</em> {props.snippet}</h5></div>
