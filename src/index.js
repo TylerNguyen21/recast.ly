bb2e73420b41fc2d908b0336e724a3da847273cc
TylerNguyen21 <tydnguyen@ucdavis.edu>
2018-11-26 17:07:08 -0800

    Installed Babel HALLELUJAH

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 // TODO: Render the `App` component to the DOM
+// import App from './compiled/components/App.js';
 import App from './components/App.js';
 
+
+ReactDOM.render(<App />, document.getElementById('app'));
